f2f81c530cdc564d24a2c93eef8f695cbbcadd36
Vincent Herbaut <vincent@ikon-k.fr>
2018-11-20 13:50:22 +0100

    Mise à jours su provider

--- src/IkcmsServiceProvider.php
+++ src/IkcmsServiceProvider.php
@@ -7,6 +7,7 @@ use Piclou\Ikcms\Console\IKCMSInstall;
 use Piclou\Ikcms\Helpers\IkForm;
 use Piclou\Ikcms\Middleware\AdministrationAccess;
 use Piclou\Ikcms\Middleware\LangMiddleware;
+use Piclou\Ikcms\Middleware\MaintenanceMiddleware;
 
 class IkcmsServiceProvider extends ServiceProvider
 {
@@ -17,10 +18,26 @@ class IkcmsServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'ikcms');
-        $this->loadViewsFrom(__DIR__.'/../resources/views', 'ikcms');
+        // Views
+        if(glob(resource_path('views/vendor/piclou/*'))) {
+            $this->loadViewsFrom(resource_path('views/vendor/piclou'), 'ikcms');
+        }else {
+            $this->loadViewsFrom(__DIR__.'/../resources/views', 'ikcms');
+        }
+        // Translations
+        if(glob(resource_path('lang/vendor/piclou/*'))) {
+            $this->loadTranslationsFrom(resource_path('lang/vendor/piclou'), 'ikcms');
+        }else {
+            $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'ikcms');
+        }
+        // Routes
+        if(glob(base_path('routes/vendor/piclou/*'))) {
+            $this->loadRoutesFrom(base_path('routes/vendor/piclou/admin.php'));
+        }else {
+            $this->loadRoutesFrom(__DIR__.'/../routes/admin.php');
+        }
+
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        $this->loadRoutesFrom(__DIR__.'/../routes/admin.php');
         $this->registerMiddleware();
 
         // Publishing is only necessary when using the CLI.
@@ -55,7 +72,7 @@ class IkcmsServiceProvider extends ServiceProvider
     {
         return ['ikcms'];
     }
-    
+
     /**
      * Console-specific booting.
      *
@@ -70,18 +87,23 @@ class IkcmsServiceProvider extends ServiceProvider
 
         // Publishing the views.
         $this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/piclou'),
+            __DIR__.'/../resources/views' => resource_path('views/vendor/piclou'),
         ], 'ikcms.views');
 
         // Publishing assets.
         $this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/piclou'),
-        ], 'ikcms.views');
+            __DIR__.'/../resources/assets' => resource_path('assets/vendor/piclou'),
+        ], 'ikcms.assets');
 
         // Publishing the translation files.
         $this->publishes([
             __DIR__.'/../resources/lang' => resource_path('lang/vendor/piclou'),
-        ], 'ikcms.views');
+        ], 'ikcms.lang');
+
+        // Publishing the route file
+        $this->publishes([
+            __DIR__.'/../routes' => base_path('routes/vendor/piclou'),
+        ], 'ikcms.routes');
 
         // Registering package commands.
         $this->commands([
@@ -97,6 +119,7 @@ class IkcmsServiceProvider extends ServiceProvider
     protected function registerMiddleware()
     {
         $this->app['router']->aliasMiddleware('ikcms.lang', LangMiddleware::class );
+        $this->app['router']->aliasMiddleware('ikcms.maintenance', MaintenanceMiddleware::class );
         $this->app['router']->aliasMiddleware('ikcms.admin', AdministrationAccess::class);
     }
 }
